f597504ef35548dea89fd7995b84d48795aee3d8
molikawa <molikawa@gmail.com>
2016-12-09 18:32:04 +0900

    alpha version

---
+++ js/back-to-top.js
@@ -0,0 +1,17 @@
+$(function() {
+	$(window).scroll(function () {
+		if ($('#mission').scrollTop() < $(this).scrollTop()) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+	$('#back-to-top').click(function () {
+		$('#back-to-top').tooltip('hide');
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+	$('#back-to-top').tooltip('show');
+});
---
+++ js/member-photo.js
@@ -0,0 +1,31 @@
+/*
+メンバー画像の切り替え
+・通常画像ファイル名の後ろ -1.jpg (画像の形式はその他 .png など)
+→ マウスオーバー時 -2.jpg (画像の形式はその他 .png など)
+*/
+$(function(){
+	$('.member-photo').hover(
+		function(){
+			$(this).attr('src', $(this).attr('src').replace('-1', '-2'));
+		}, 
+		function(){
+			$(this).attr('src', $(this).attr('src').replace('-2', '-1'));
+		}
+	);
+});
+
+/*
+上記のための切り替え画像のプリロード
+*/
+$(function(){
+  $('.members-photo').each(function(){
+    if(String($(this).attr("src")).match(/_1\.(.*)$/)){
+      var img = new Image();
+      img.src = String($(this).attr("src")).replace(/_1\.(.*)$/,"_2.$1");
+    }
+  });
+});
+
+
+
+
