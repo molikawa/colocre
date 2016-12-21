1448a3e1dff87196d86a4791047dcc9a5f30eaef
molikawa <molikawa@gmail.com>
2016-12-21 18:59:13 +0900

    1.0

--- js/bg-changer.js
+++ js/bg-changer.js
@@ -1,7 +1,8 @@
+/* 読み込み順は登場順 */
 $('<img>').attr('src', 'img/bg-default.jpg');
-$('#body-wrapper').append('<div id="bg-sec0"></div>');
 $('<img>').attr('src', 'img/bg-mission.jpg');
 $('<img>').attr('src', 'img/bg-works.jpg');
+$('<img>').attr('src', 'img/bg-way.jpg');
 $('<img>').attr('src', 'img/bg-rolls.jpg');
 $('<img>').attr('src', 'img/bg-members.jpg');
 $('<img>').attr('src', 'img/bg-news.jpg');
@@ -9,19 +10,18 @@ $(function() {
   var secTops = new Array();
   var navClickFlug = false;
   var pageHeight = $('body').outerHeight(true);
-  var bgImages = new Array('bg-default.jpg',  /* bg0: header */
-              'bg-mission.jpg',  /* bg1: bg-mission.jpg */
-              'bg-works.jpg',    /* bg2: bg-works.jpg */
-              'bg-rolls.jpg',    /* bg3: bg-rolls.jpg */
-              'bg-members.jpg',  /* bg4: bg-members.jpg */
-              'bg-news.jpg',    /* bg5: bg-news.jpg */
-              'bg-default.jpg');  /* bg6: bg-default.jpg for contact  */
+  var bgImages = new Array('bg-default.jpg',/* bg0 (title)*/
+                  'bg-mission.jpg',         /* bg1 */
+                  'bg-works.jpg',           /* bg2 */
+                  'bg-way.jpg',             /* bg3 */
+                  'bg-rolls.jpg',           /* bg4 */
+                  'bg-members.jpg',         /* bg5 */
+                  'bg-news.jpg',            /* bg6 */
+                  'bg-default.jpg');        /* bg7 (Contact) */
   secTops[0] = $('header').offset().top;
   $('section').each(function (i) {
     secTops[i+1] = $(this).offset().top;
   });
-  /*secTops[secTops.length] = $('footer').offset().top;*/
-  
   var timer = false;
   $(window).resize(function() {
     if (false != timer) {
@@ -35,8 +35,8 @@ $(function() {
       });
     }, 200);
   });
-  
-  var curSecNum = -1;
+  var curSecNum = 0;
+  $('#body-wrapper').css('background', 'url("img/bg-default.jpg")');
   $(window).scroll(function () {
     for (var i = secTops.length-1; 0 <= i; --i) {
       if ((secTops[i] - 200) < $(window).scrollTop()) {
@@ -46,7 +46,8 @@ $(function() {
     }
   });
   function changeBgImage(secNum) {
-    if ((secNum != curSecNum) && (false == navClickFlug)) {
+    if ((curSecNum != secNum) && (false == navClickFlug)) {
+      $('#body-wrapper').css('background', 'url("img/'+bgImages[secNum]+'")');
       $('#bg-sec'+curSecNum)
         .fadeOut(500, function() {
           $(this).remove();
